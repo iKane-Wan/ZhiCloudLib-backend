f762d688853bb452c8c2e61dd27b601db4f92ac3
Kane <kanewan103@gmail.com>
2026-02-25 18:20:37 +0800

    fix:修复因日志依赖导致service-ai不能启动的问题，网关无法转发service-ai接口问题

--- lib-services/service-ai/src/main/java/com/kane/controller/AiController.java
+++ lib-services/service-ai/src/main/java/com/kane/controller/AiController.java
@@ -4,9 +4,11 @@ import com.kane.R;
 import com.kane.entity.bo.BookBO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/ai")
 public class AiController {
 
     @PostMapping
